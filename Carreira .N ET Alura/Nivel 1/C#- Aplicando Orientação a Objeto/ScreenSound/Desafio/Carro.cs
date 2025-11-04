d4717d2693388fefd5a0b9f67bdaee512874a7a0
Guilherme Machado <guilherme.machado@bebidaspoty.com>
2025-11-04 00:17:08 -0300

    Projeto Carreira .NET

---
+++ Carreira .N ET Alura/Nivel 1/C#- Aplicando Orientação a Objeto/ScreenSound/Desafio/Carro.cs
@@ -0,0 +1,41 @@
+﻿namespace ScreenSound.Desafio;
+public class Carro
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public string Cor { get; set; }
+    public int QuantidadePortas { get; set; }
+    public int PotenciaMotor { get; set; }
+    public string AnoFabricacao { get; set; }
+    public int Velocidade { get; set; }
+
+    public string DescricaoDetahada => $"{Marca} {Modelo} {Cor} {QuantidadePortas} portas {PotenciaMotor} HP {AnoFabricacao}";
+    public void exibirInformacoes()
+    {
+        Console.WriteLine($"Informações do carro: {this.Marca} {this.Modelo}, {this.QuantidadePortas} portas, {this.AnoFabricacao}");
+    }
+
+    public void acelerar()
+    {
+        Console.WriteLine("Acelerando...");
+        if (Velocidade < 100)
+        {
+            Velocidade = Velocidade + 5;
+        }
+    }
+
+    public void frear()
+    {
+        Console.WriteLine("Freando...");
+        if (Velocidade > 0)
+        {
+            Velocidade = Velocidade - 5;
+        }
+    }
+
+    public void buzinar()
+    {
+        Console.WriteLine("Bi Bi");
+    }
+
+}
---
+++ Carreira .N ET Alura/Nivel 1/C#- Aplicando Orientação a Objeto/ScreenSound/Desafio/ContaBancaria.cs
@@ -0,0 +1,15 @@
+﻿namespace ScreenSound.Desafio;
+public class ContaBancaria
+{
+    public string Titular { get; set; }
+    public int NumeroIndicador { get; set; }
+    public double Saldo { get; set; }
+    public string Senha { get; set; }
+
+    public void GetDetalhesConta()
+    {
+        Console.WriteLine($"Titular: {Titular}");
+        Console.WriteLine($"Número Indicador: {NumeroIndicador}");
+        Console.WriteLine($"Saldo: R$ {Saldo}");
+    }
+}
