0a3e46f712e2800b2a00218561936382c5be22ba
Guilherme Machado <guilherme.machado@bebidaspoty.com>
2025-11-17 15:34:50 -0300

    Praticando C# - Orientação a objetos com encapsulamento

---
+++ Carreira .N ET Alura/Nivel 1/Praticando C# - Orientação a objetos com encapsulamento/PraticandoCSharpEncaps/Produto.cs
@@ -0,0 +1,43 @@
+﻿public class Produto
+{
+    // Atributo privado
+    private string imagem;
+
+    // Propriedades autoimplementadas
+    public string Nome { get; }
+    public string Descricao { get; }
+    public decimal Preco { get; private set; } // Somente leitura fora da classe
+    public int Estoque { get; }
+
+    // Propriedade com lógica para o get e set
+    public string Imagem
+    {
+        get
+        {
+            return imagem;
+        }
+        set
+        {
+            if (value.Length > 0)
+            {
+                this.imagem = value;
+            }
+        }
+    }
+
+    // Construtor
+    public Produto(string nome, string descricao, decimal preco, int estoque, string imagem)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Preco = preco;
+        Estoque = estoque;
+        this.Imagem = imagem; // Usando a propriedade Imagem para garantir a validação
+    }
+
+    // Método para alterar o preço com desconto (só pode ser acessado dentro da classe)
+    public void AlterarPrecoComDesconto(decimal desconto)
+    {
+        Preco = Preco - (Preco * desconto); // Acesso direto à propriedade Preco
+    }
+}
---
+++ Carreira .N ET Alura/Nivel 1/Praticando C# - Orientação a objetos com encapsulamento/PraticandoCSharpEncaps/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Criando um objeto da classe Produto
+        Produto item1 = new Produto("Produto 1", "Descrição do Produto 1", 19.99m, 10, "imagem1.jpg");
+
+        // Acessando as propriedades do objeto
+        Console.WriteLine($"Nome: {item1.Nome}");
+        Console.WriteLine($"Descricao: {item1.Descricao}");
+        Console.WriteLine($"Preco: {item1.Preco}");
+        Console.WriteLine($"Estoque: {item1.Estoque}");
+        Console.WriteLine($"Imagem: {item1.Imagem}");
+
+        // Alterando a imagem usando a propriedade
+        item1.Imagem = "nova_imagem.jpg";
+        Console.WriteLine($"Nova Imagem: {item1.Imagem}");
+
+        // Alterando o preço com desconto usando o método
+        item1.AlterarPrecoComDesconto(0.1m); // 10% de desconto
+        Console.WriteLine($"Preço com desconto: {item1.Preco}");
+    }
+}
